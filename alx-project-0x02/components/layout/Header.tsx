5422b72ec73777ef6d3e639e6642fcff8e3c5c74
Hemen Solomon <hemensolomon83@gmail.com>
2025-07-26 13:05:09 +0300

    Add routing pages and navigation link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white p-4">
+    <header className="bg-blue-600 text-white p-4 flex space-x-6 items-center">
       <h1 className="text-2xl font-bold">ALX Project Header</h1>
+      <nav className="flex space-x-4 ml-8">
+        <Link href="/home" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+      </nav>
     </header>
   );
 };
